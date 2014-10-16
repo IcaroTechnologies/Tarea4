5437fc60ba9e3c88d7bd822aa6c8ee013a166dbd
Teter <Teter@192.168.1.134>
2014-10-16 15:43:52 -0430

    Repositorio Segundo Metodo

---
+++ SAGE/intentarEstacionar.py
@@ -0,0 +1,17 @@
+'''
+Created on Oct 16, 2014
+
+@author: Teter
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        
---
+++ SAGE/testIntentarEstacionar.py
@@ -0,0 +1,18 @@
+'''
+Created on Oct 16, 2014
+
+@author: Teter
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
