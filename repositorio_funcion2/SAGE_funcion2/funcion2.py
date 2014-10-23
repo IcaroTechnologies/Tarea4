023d9366566471874fcc88bd55ef4c93b0025178
Teter <Teter@localhost>
2014-10-23 16:09:57 -0430

    Prueba inicial de la función 2, sin implementar código.

---
+++ repositorio_funcion2/SAGE_funcion2/funcion2.py
@@ -0,0 +1,17 @@
+'''
+Created on Oct 23, 2014
+
+@author: Teter
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
