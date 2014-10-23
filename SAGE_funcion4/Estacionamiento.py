932af37a9fff34cc377465d25d1237ca8247f060
Teter <Teter@localhost>
2014-10-23 18:10:19 -0430

    commit inicial

---
+++ SAGE_funcion4/Estacionamiento.py
@@ -0,0 +1,14 @@
+'''
+Created on Oct 23, 2014
+
+@author: Luis Miglietti, Aberlardo Valino
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+
+        
---
+++ SAGE_funcion4/testfuncion4.py
@@ -0,0 +1,18 @@
+'''
+Created on Oct 23, 2014
+
+@author: Luis Miglietti, Abelardo Valino
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
