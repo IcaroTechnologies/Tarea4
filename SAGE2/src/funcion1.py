2eaa551c2f390ba6e0ca22b2abf6be904a9d42a5
Nelson Avelino <Nelson Avelino@NelsonSaturno.mshome.net>
2014-10-23 16:37:59 -0430

    Commit inicial

---
+++ SAGE2/src/funcion1.py
@@ -0,0 +1,17 @@
+'''
+Created on 23/10/2014
+
+@author: Nelson Avelino
+'''
+
+class estacionamiento(object):
+    '''
+    classdocs
+    '''
+    
+    
+    def reservarPuesto(self,estadoEstacionamiento,tiempoReservado,placa,placaPuesto,PlacaEntrada):
+        return True
+        
+
+        
---
+++ SAGE2/src/pruebaReservarPuesto.py
@@ -0,0 +1,18 @@
+'''
+Created on 23/10/2014
+
+@author: Nelson Avelino
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
