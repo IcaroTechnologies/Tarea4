eac7c2caab0f5836ef07cd027657d7e05bede8f1
Teter <Teter@localhost>
2014-10-23 18:12:59 -0430

    Parametros

--- SAGE_funcion4/Estacionamiento.py
+++ SAGE_funcion4/Estacionamiento.py
@@ -4,10 +4,11 @@ Created on Oct 23, 2014
 @author: Luis Miglietti, Aberlardo Valino
 '''
 
-class MyClass(object):
-    '''
-    classdocs
-    '''
+class Estacionamiento(object):
+    
+    def desocuparPuesto(self, estadoEstacionamiento, placa, tiempoSalida, placaPuesto):  
+        return True
+
 
 
 
--- SAGE_funcion4/testfuncion4.py
+++ SAGE_funcion4/testfuncion4.py
@@ -4,15 +4,22 @@ Created on Oct 23, 2014
 @author: Luis Miglietti, Abelardo Valino
 '''
 import unittest
+import datetime
+from Estacionamiento import Estacionamiento
 
 
 class Test(unittest.TestCase):
 
 
-    def testName(self):
-        pass
-
-
+    ''' Prueba inicial que falló, debido a que el procedimiento no retornaba ningun valor'''
+    def testParametros(self):
+        est=Estacionamiento()
+        estadoEstacionamiento = []
+        placa="AWW7"
+        horaSalida=datetime.timedelta(hours=12)
+        placaPuesto ={}
+        self.assertTrue(est.desocuparPuesto(estadoEstacionamiento,placa,horaSalida,placaPuesto))   
+        
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
